0dce8980db5160da180492e43190c1cae51028fa
CFM503 <pzeus@163.com>
2026-05-21 08:38:33 +0800

    chore: bump version to v1.4.3

--- cmd/gopass/main.go
+++ cmd/gopass/main.go
@@ -19,7 +19,7 @@ func main() {
 	flag.Parse()
 
 	fmt.Println("=== GoPass 透明代理 ===")
-	fmt.Println("Version: v1.4.2")
+	fmt.Println("Version: v1.4.3")
 	fmt.Printf("PID: %d\n\n", os.Getpid())
 
 	// Load Configuration
