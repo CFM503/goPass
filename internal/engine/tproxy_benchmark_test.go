75dc52f0c1395b5249d2031643531b03bfe76722
Jerry Cat <pzeus@163.com>
2026-09-10 13:21:35 +0800

    test: benchmark revised relay buffer implementation

--- internal/engine/tproxy_benchmark_test.go
+++ internal/engine/tproxy_benchmark_test.go
@@ -9,80 +9,40 @@ import (
 func runProxyBufferBenchmark(b *testing.B, bufSize int) {
 	b.ReportAllocs()
 	b.SetBytes(int64(bufSize))
-
 	srcR, srcW := net.Pipe()
 	dstR, dstW := net.Pipe()
-
 	done := make(chan struct{})
 
-	// Discard consumer for dst
 	go func() {
 		sink := make([]byte, 1024*1024)
-		for {
-			_, err := dstR.Read(sink)
-			if err != nil {
-				return
-			}
-		}
+		for { if _, err := dstR.Read(sink); err != nil { return } }
 	}()
-
-	// Feeder for src
 	data := make([]byte, bufSize)
 	go func() {
 		defer close(done)
-		for i := 0; i < b.N; i++ {
-			if _, err := srcW.Write(data); err != nil {
-				return
-			}
-		}
-		srcW.Close()
+		for i := 0; i < b.N; i++ { if _, err := srcW.Write(data); err != nil { return } }
+		_ = srcW.Close()
 	}()
 
 	b.ResetTimer()
-
-	buf := bufferPool.Get().([]byte)
-	defer bufferPool.Put(buf)
-	if cap(buf) < bufSize {
-		buf = make([]byte, bufSize)
-	}
-	copyBuf := buf[:bufSize]
-
+	buf := getRelayBuffer(bufSize)
+	defer putRelayBuffer(buf)
 	var transferred int64
 	for {
-		n, err := srcR.Read(copyBuf)
+		n, err := srcR.Read(buf)
 		if n > 0 {
 			atomic.AddInt64(&transferred, int64(n))
-			if _, werr := dstW.Write(copyBuf[:n]); werr != nil {
-				break
-			}
+			if _, werr := dstW.Write(buf[:n]); werr != nil { break }
 		}
-		if err != nil {
-			break
-		}
+		if err != nil { break }
 	}
-
 	b.StopTimer()
-	_ = dstW.Close()
-	_ = dstR.Close()
+	_ = srcR.Close(); _ = dstW.Close(); _ = dstR.Close()
 	<-done
 }
 
-func BenchmarkProxyBuffer32K(b *testing.B) {
-	runProxyBufferBenchmark(b, 32*1024)
-}
-
-func BenchmarkProxyBuffer64K(b *testing.B) {
-	runProxyBufferBenchmark(b, 64*1024)
-}
-
-func BenchmarkProxyBuffer256K(b *testing.B) {
-	runProxyBufferBenchmark(b, 256*1024)
-}
-
-func BenchmarkProxyBuffer512K(b *testing.B) {
-	runProxyBufferBenchmark(b, 512*1024)
-}
-
-func BenchmarkProxyBuffer1M(b *testing.B) {
-	runProxyBufferBenchmark(b, 1024*1024)
-}
+func BenchmarkProxyBuffer32K(b *testing.B) { runProxyBufferBenchmark(b, 32*1024) }
+func BenchmarkProxyBuffer64K(b *testing.B) { runProxyBufferBenchmark(b, 64*1024) }
+func BenchmarkProxyBuffer256K(b *testing.B) { runProxyBufferBenchmark(b, 256*1024) }
+func BenchmarkProxyBuffer512K(b *testing.B) { runProxyBufferBenchmark(b, 512*1024) }
+func BenchmarkProxyBuffer1M(b *testing.B) { runProxyBufferBenchmark(b, 1024*1024) }
